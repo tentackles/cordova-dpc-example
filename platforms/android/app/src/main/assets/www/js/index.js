2995366e686cdcd1aea83b3ddd2c674efb48ee4f
Apurva Ojas <apurvaojas10@gmail.com>
2019-03-16 22:22:57 +0530

    commit

--- platforms/android/app/src/main/assets/www/js/index.js
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -35,6 +35,7 @@ var app = {
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
+        DPC.on("testEvent", alert);
         DPC.greet("Hello", alert, alert);
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
