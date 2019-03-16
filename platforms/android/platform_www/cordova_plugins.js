499c9c5c373dd9abf262cc3712047e8b9deb9005
Apurva Ojas <apurvaojas10@gmail.com>
2019-03-16 19:56:31 +0530

    commit

---
+++ platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,19 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+  {
+    "id": "com.tentackles.plugins.DPC.DPC",
+    "file": "plugins/com.tentackles.plugins.DPC/www/DPC.js",
+    "pluginId": "com.tentackles.plugins.DPC",
+    "clobbers": [
+      "DPC"
+    ]
+  }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+  "cordova-plugin-whitelist": "1.3.3",
+  "com.tentackles.plugins.DPC": "0.0.1"
+};
+// BOTTOM OF METADATA
+});
